f909b364d0648ff8bf15a31037ecc99fac789b2d
Avan Suwandana <54794101+AvanSuwandana@users.noreply.github.com>
2024-12-06 13:13:50 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// MongoDB Connection
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.2hqmk.mongodb.net/vistaVoyage?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Middleware
+app.use(express.json()); // Parsing JSON
+app.use(express.static(path.join(__dirname, "../frontend"))); // Sajikan file frontend
+
+// Route Default
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+// Import Routes
+const tourRoutes = require("./routes/tourRoutes");
+app.use("/api/tours", tourRoutes); // CRUD untuk Tour
+
+// Jalankan Server
+const PORT = 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
